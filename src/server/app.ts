665388632d124104f7e400619283d9ac242ada24
oriolraventosc <oriol.raventos.c@gmail.com>
2022-11-09 19:31:17 +0100

    Create users router

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import usersRouter from "./routers/usersRouter.js";
 
 const app = express();
 
@@ -8,5 +9,9 @@ app.disable("x-powered-by");
 app.use(express.json());
 
 app.use(morgan("dev"));
+
+app.use("/users", usersRouter);
+
+app.use();
 
 export default app;
