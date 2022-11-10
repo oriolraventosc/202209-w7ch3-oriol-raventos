7dad8f114dbc96b6f67b19d32961f86251d6293d
oriolraventosc <oriol.raventos.c@gmail.com>
2022-11-10 01:05:36 +0100

    Add schema validation for userRegister endpoint

--- src/server/routers/usersRouter.ts
+++ src/server/routers/usersRouter.ts
@@ -1,9 +1,11 @@
+import { validate } from "express-validation";
 import express from "express";
 import { userRegister } from "../controllers/usersController.js";
+import userRegisterValidation from "../schemas/userRegisterSchema.js";
 
 // eslint-disable-next-line new-cap
 const usersRouter = express.Router();
 
-usersRouter.post("/register", userRegister);
+usersRouter.post("/register", validate(userRegisterValidation), userRegister);
 
 export default usersRouter;
